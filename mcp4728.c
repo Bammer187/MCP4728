ac4c76db87eec5f76343d259012b9b435070a0c1
Lukas Bammer <lukas.bammer@bulme.at>
2026-02-12 08:28:58 +0100

    Created sw_i2c_read_byte()

--- mcp4728.c
+++ mcp4728.c
@@ -24,3 +24,26 @@ static void sw_i2c_write_byte(gpio_num_t scl, gpio_num_t sda, gpio_num_t ldac, u
     gpio_set_level(scl, 0);
     gpio_set_direction(sda, GPIO_MODE_INPUT_OUTPUT);
 }
+
+
+static uint8_t sw_i2c_read_byte(gpio_num_t scl, gpio_num_t sda, uint32_t delay, bool ack) {
+    uint8_t byte = 0;
+    gpio_set_direction(sda, GPIO_MODE_INPUT);
+    
+    for (int i = 0; i < 8; i++) {
+        ets_delay_us(delay);
+        gpio_set_level(scl, 1);
+        if (gpio_get_level(sda)) byte |= (0x80 >> i);
+        ets_delay_us(delay);
+        gpio_set_level(scl, 0);
+    }
+
+    // ACK Phase
+    gpio_set_direction(sda, GPIO_MODE_OUTPUT);
+    gpio_set_level(sda, ack ? 0 : 1);
+    ets_delay_us(delay);
+    gpio_set_level(scl, 1);
+    ets_delay_us(delay);
+    gpio_set_level(scl, 0);
+    return byte;
+}
